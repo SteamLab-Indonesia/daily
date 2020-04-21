44e6794c0f1d6c40f62c08ad264085f8140ebe71
Yvonne Tansu <yvonne.tansu@gmail.com>
2020-04-21 16:25:44 +0700

    App changes

--- App.js
+++ App.js
@@ -1,71 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  TextInput
-} from 'react-native';
-
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './screens/SplashScreen';
+import Login from './screens/Login';
 import MainTabNavigator from './screens/MainTabNavigator';
 
-function LoginScreen({ navigation }) {
-	return (
-	  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-		<Text>Login Screen</Text>
-		<TextInput keyboardType="email-address" />
-		<Button
-		  title="Go to Main"
-		  onPress={() => navigation.navigate('Main')}
-		/>
-	  </View>
-	);
-}
-
-function DetailsScreen(props) {
-	return (
-	  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-		<Text>Details Screen</Text>
-		<Button
-		  title="Go to Details... again"
-		  onPress={() => props.navigation.push('Details')}
-		/>
-		<Button title="Go to Home" onPress={() => props.navigation.navigate('Home')} />
-		<Button title="Go back" onPress={() => props.navigation.goBack()} />
-	  </View>
-	);
+function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
+    );
   }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
 class App extends Component {
+    render(){
+        return(
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />  
+            <Stack.Screen name="Main" component={MainTabNavigator} options={{headerShown: false}}/>  
+        </Stack.Navigator>
+        </NavigationContainer>
+        )
+    }
+}
 
-	render() {
-		return (
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}} />
-					<Stack.Screen name="Login" component={LoginScreen} />
-					<Stack.Screen name="Details" component={DetailsScreen} />
-					<Stack.Screen name="Main" component={MainTabNavigator} />
-				</Stack.Navigator>
-		  </NavigationContainer>
-		)
-	}
-};
+const styles ={
+    splashscr: {
+        backgroundColor: 'lightgreen',
+        fontSize: 70,
+        fontWeight: 4,
+        fontColor: 'darkgreen'
+    },
 
+}
 export default App;
