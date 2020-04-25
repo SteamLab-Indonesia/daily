b0f141a04ddddb94075faf9c6dbb68ad09fcff21
hs <hs@hss-MacBook-Pro-106.local>
2020-04-25 11:59:05 +0700

    Reminder List add Category

--- screens/Reminder.js
+++ screens/Reminder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import {Picker, Dimensions} from 'react-native'
 import { Container, Header, Content, List, ListItem, Text, Button, Icon, Left, Body, Right, Switch, CheckBox,
    Textarea, View,Fab  } from 'native-base';
 import DialogManager,{ DialogComponent } from 'react-native-dialog-component'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import DialogButton from 'react-native-dialog-component/dist/components/DialogButton';
-import {getReminder, getIncomeCategory, getExpenseCategory, updateReminder, insertReminder, reminderDate} from  '../libs/database';
+import {getReminder, getCategory, updateReminder, insertReminder, reminderDate} from  '../libs/database';
 import { format } from "date-fns";
 
 
@@ -12,12 +13,7 @@ class ReminderList extends Component {
     state={
         itemList:{}, // id,date,task,complete
         open: false,
-        category: [
-            {name : 'School'},
-            {name : 'Housecores'},
-            {name : 'Leisure'},
-            {name : 'Project'},
-            {name : 'Uncategorised'}],
+        category: [],
         selectedCat: '',
         reminder: '',
         selected: false
@@ -25,44 +21,77 @@ class ReminderList extends Component {
 
 componentDidMount = () =>{
   getReminder().then((data) => {
-      this.setState({itemList:data})
+    this.setState({itemList:data})
   })
-  updateReminder
+  getCategory().then((data)=>{
+    this.setState({category:data})
+  })
 }
 
-checkboxPress = (index) => {
+checkboxPress = (key,index) => {
     let {itemList} =this.state;
-    itemList[index].complete = !itemList[index].complete;
+    itemList[key][index].complete = !itemList[key][index].complete;
     this.setState({itemList})
+    console.log(itemList)
 }
 
-selectedPress = (index) => {
-  let {selected} =this.state;
-  selected = !selected;
-  this.setState({selected})
-}
+onChangeCategoryPress = ((value) =>{ //simpan di state
+  let selectedCat = this.state.selectedCat;
+  selectedCat=value;
+  this.setState({selectedCat});
+  console.log(this.state.selectedCat)
+})
+
+// selectedPress = (index) => {
+//   let {selected} =this.state;
+//   selected = !selected;
+//   this.setState({selected})
+// }
 
 handleSave = () => {
+  let {itemList, reminder, category} = this.state;
   this.dialogComponent.dismiss()
-  insertReminder({
-    reminder: this.state.reminder,
-    // category: this.state.category
-  }).then(() => {
-    if(this.props.updateReminder) //kl updateCategory di TransactionList deleted, gbs jln
-      this.props.updateReminder()
+  insertReminder({ //save di database
+    date: new Date(),
+    task: reminder,
+    complete: false,
+    category: category
+  }).then((id)=>{
+    itemList['date'].push({name: reminder, complete: false, date: new Date(), id:id, category: category})
+    this.setState({itemList}) //tampilan di hp
   })
 }
+//   if (itemList['date'][index] != '')
+//     {
+//       updateReminder(itemList['date'][index].id, itemList['date'][index])
+//     }
+//     // else
+//     // {
+//     //   insertReminder(itemList['date'][index]).then((id) => {
+//     //     collate[index].id = id;
+//     //     this.setState({collate});
+//     //   })
+//     // }    
+// }
+
+// handleSaveCategory = () => {
+//   this.dialogComponent.dismiss()
+//   insertCategory({
+//     date: new Date(),
+//     task: this.state.reminder,
+//     complete: false
+//   }).then((id)=>{
+//     itemList['date'].push({name: this.state.reminder, complete: false, date: new Date(), id:id})
+//     this.setState({itemList})
+//   })
+// }
 
 handleClose = () => {
   this.setState({open:false})
 }
 
-onChangeReminder =(event) => {
-  this.setState({reminder: event.target.value});
-}
-
-onChangeCategory = (event) => {
-  this.setState({selectedCat: event.target.value});
+onChangeReminder =(value) => {
+  this.setState({reminder: value});
 }
 
   render() {
@@ -70,32 +99,31 @@ onChangeCategory = (event) => {
     return (
         <Container style = {{backgroundColor :'#1B2732'}}>
           <Content>
-            <List>
+            
               {
-                Object.keys(itemList).map((key) =>{
-                  return (
-                    <List>
-                      <ListItem itemDivider >
-                        <Text>{reminderDate(key)}</Text>
-                      </ListItem>
+                Object.keys(itemList).map((date) =>{
+                  return(
+                  <List>
+                    <ListItem itemDivider >
+                      <Text>{reminderDate(date)}</Text>
+                    </ListItem>
                       {
-                        itemList[key].map((item,index)=>{
-                            return (
-                            <ListItem key = {index}>
-                                <Left>
-                                  <Text style={item.complete ? {textDecorationLine: 'line-through'} : null} style={{ color: '#bcc6cf', fontSize : 18}}>{item.name}</Text>
-                                </Left>
-                                <Right>
-                                  <CheckBox checked={item.complete} onPress={()=>this.checkboxPress(index)}/>
-                                </Right>
-                            </ListItem>
-                        )})
-                      } 
-                    </List>                   
-                  )
+                      itemList[date].map((item,index)=>{
+                          return (
+                          <ListItem key = {index}>
+                              <Left>
+                              <Text style={item.complete ? {textDecorationLine: 'line-through'} : null} style={{ color: '#bcc6cf', fontSize : 18}}>{item.name}</Text>
+                              </Left>
+                              <Right>
+                              <CheckBox checked={item.complete} onPress={()=>this.checkboxPress(date,index)}/>
+                              </Right>
+                          </ListItem>
+                          )}
+                      )}
+                    </List>)
                 })
               }
-            </List>            
+            
           </Content>
               {/* <Button onClick = {()=>this.setState({open:true})}>
                 <Icon name='add-circle-outline'></Icon>
@@ -116,10 +144,36 @@ onChangeCategory = (event) => {
             <Text>
               Add New Reminder
             </Text>
-            <Textarea rowSpan={5} bordered placeholder="Textarea" onChange = {(event) => this.onChangeReminder(event)}/>
+            <Textarea rowSpan={5} bordered placeholder="Textarea" onChangeText = {(text) => this.onChangeReminder(text)}/>
+          </View>
+          <View>
+            <Picker
+              selectedValue={this.state.selectedCat}
+              style={{ height: 50, width: 150 }}
+              onValueChange = {(event) => this.onChangeCategoryPress(event)}
+            >
+              {category.map((item)=>{
+                return <Picker.Item label={item.name} value={item.id} />
+              })}
+            </Picker>
+            <Button onPress={()=>this.dialogComponent2.show()} style={{width:60, backgroundColor:'gray'}}>
+              <Icon name='add-circle' />
+            </Button>
+            <DialogComponent
+              ref={(dialogComponent2) => { this.dialogComponent = dialogComponent2; }}
+            >
+              <View>
+                <Text>
+                  Add New Category
+                </Text>
+                <Textarea rowSpan={3} bordered placeholder="Input New Category"/>
+              </View>
+              <DialogButton text = 'Cancel' onPress={()=>this.dialogComponent.dismiss()} color="primary" />
+              <DialogButton text = 'Save' onPress={this.handleSaveCategory} color="primary" />
+            </DialogComponent>
           </View>
           <DialogButton text = 'Cancel' onPress={()=>this.dialogComponent.dismiss()} color="primary" />
-          <DialogButton text = 'Save' onPress={()=>this.dialogComponent.dismiss()} color="primary" />
+          <DialogButton text = 'Save' onPress={this.handleSave} color="primary" />
         </DialogComponent>
               
               
